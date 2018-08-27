d164fd82a794d18aee14637b13ebc758531b4dca
Tim <tgosnell@Administrators-MacBook-Pro.local>
2018-08-27 12:53:01 -0600

    added test placeholders, dynamodb permissions

--- handler.spec.js
+++ handler.spec.js
@@ -12,3 +12,30 @@ describe("The handler function", () => {
         });
     });
 });
+
+describe("The handler function", () => {
+    it("returns a message", () => {
+        handler.create(undefined, undefined, function(error, response){
+            let body = JSON.parse(response.body);
+            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
+        });
+    });
+});
+
+describe("The handler function", () => {
+    it("returns a message", () => {
+        handler.update(undefined, undefined, function(error, response){
+            let body = JSON.parse(response.body);
+            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
+        });
+    });
+});
+
+describe("The handler function", () => {
+    it("returns a message", () => {
+        handler.delete(undefined, undefined, function(error, response){
+            let body = JSON.parse(response.body);
+            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
+        });
+    });
+});
